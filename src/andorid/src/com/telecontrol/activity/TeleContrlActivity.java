be823d1720c68d6e54e192af2b3f27122a337621
zzx_xuan <xiaoxuanfengzzx@163.com>
2014-11-02 17:45:35 +0800

    Signed-off-by: zzx_xuan <xiaoxuanfengzzx@163.com>

--- src/andorid/src/com/telecontrol/activity/TeleContrlActivity.java
+++ src/andorid/src/com/telecontrol/activity/TeleContrlActivity.java
@@ -2,7 +2,9 @@ package com.telecontrol.activity;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.view.Window;
 
 public class TeleContrlActivity extends Activity {
 	private String ip;
@@ -15,6 +17,10 @@ public class TeleContrlActivity extends Activity {
 		Intent intent=getIntent();
 		ip=intent.getStringExtra("ip");
 		port=intent.getIntExtra("port", 54322);
+		requestWindowFeature(Window.FEATURE_NO_TITLE);//隐藏标题
+		 if(getRequestedOrientation()!=ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE){
+			  setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+			 }
 		setContentView(new TeleScrenSurfaceView(this,ip,port));
 	}
 }
